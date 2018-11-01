dd123e2aa6d408ee18623ee1a764d7c905f6d543
Bilalbc <44500503+Bilalbc@users.noreply.github.com>
2018-11-01 10:57:06 -0400

    Add files via upload

---
+++ Stock Market Anaysis/src/formulas.java
@@ -0,0 +1,59 @@
+import java.util.*; 
+import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+public class formulas {
+	readFile r = new readFile();
+	//Date	Open	High	Low	Close*	Adj Close**	Volume
+	Date [] dateArray = new Date[r.findRows()];
+	double [][] values = new double [r.findRows()][7];
+	
+	public void createDateArray(String [][] n) {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		for(int i=0;i<n.length;i++)
+			for(int j=0; j<n[i].length;j++) {
+				if(j==0) {
+					try {
+						Date newDate = (Date)df.parse(n[i][0]);
+						dateArray[i] = newDate;
+					}
+					catch(Exception e) {
+						System.out.println(e);
+					}
+				}
+			}
+	}
+	public void createValues(String[][] n) {
+		for (int i=0; i<n.length;i++) {
+			for(int j=0;j<n[i].length;j++) {
+				if(j>0){
+					values[i][j] = Float.parseFloat(n[i][j]);
+				}else {
+					values[i][j]=0;
+				}
+			}
+		}
+	}
+	
+	public void printArray() {
+		for(int i=0; i<values.length;i++) {
+			for(int j=0; j<values[i].length;j++) {
+				System.out.printf("%.2f ", values[i][j]);
+			}
+			System.out.println();
+		}
+		
+	}
+	public void reverseArray(double [][] r) {
+		for(int i=0;i<10/2;i++) {
+		    r[i] = r[10 - i - 1];	    	
+		}
+	
+			
+	}
+	
+	public double[][] getArray(){
+		return values;
+	}
+}
---
+++ Stock Market Anaysis/src/stockMarketAnalysis.java
@@ -0,0 +1,27 @@
+
+public class stockMarketAnalysis {
+
+	public static void main(String[]args) {
+		
+		
+		
+		readFile r = new readFile();
+		r.makeArray();
+		//r.printArray();
+		
+		formulas f = new formulas();
+		f.createValues(r.getArray());
+		f.printArray();
+		System.out.println();
+		f.reverseArray(f.getArray());
+		f.printArray();
+		
+		
+		 for(int i=0;i<10;i++){ 
+			// System.out.println(f.values[i][3]);
+		 }
+		 
+		 drawGraphs.main(args);
+		
+	}
+}
