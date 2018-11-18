091d039b7c4fd7cca0b422fa6db43109b94b28ed
Bilalbc <44500503+Bilalbc@users.noreply.github.com>
2018-11-18 14:08:03 -0800

    Add files via upload

--- Stock Market Anaysis/src/subPlot.java
+++ Stock Market Anaysis/src/subPlot.java
@@ -1,12 +1,13 @@
 package stockAnalysisProgram;
 
-import java.awt.Font;
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.annotations.XYTextAnnotation;
 import org.jfree.chart.axis.AxisLocation;
 import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.DateTickMarkPosition;
@@ -30,67 +31,95 @@ import stockAnalysisProgram.readFile;
 
 public class subPlot extends ApplicationFrame {
 
-    public subPlot(final String title) {
-
-        super(title);
-        final JFreeChart chart = createCombinedChart();
-        final ChartPanel panel = new ChartPanel(chart, true, true, true, false, true);
-        panel.setPreferredSize(new java.awt.Dimension(600, 600));
-        setContentPane(panel);
-
-    }
-
 	private String fileLoc;
 	private String title;
 	private int period = 0;
 	private formulas f;
 	private int frame = 20;
 	
-    private JFreeChart createCombinedChart() {
-    	readFile r;
-
-		fileLoc = "C:\\Users\\haseebchaudhry\\git\\repository\\stockAnalysisProgram\\src\\Data\\Monthly - 5 years.txt";
-		r = new readFile(fileLoc);
-		f = new formulas(fileLoc);
-		r.makeArray(); 
-		f.createValues(r.getArray());
-		f.createDateArray(r.getArray());
-		f.createSMA(frame);
-			period = f.values.length;
-			title = "monthly - 5 years";
+    public subPlot(String applicationTitle, int choice ) {
     	
+    		super(applicationTitle);
+    		JFreeChart chart = createChart(choice);
+    	    ChartPanel panel = new ChartPanel(chart, true, true, true, true, true);
+    	    panel.setPreferredSize(new java.awt.Dimension(600, 600));
+    	    setContentPane(panel);
+    		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+    
+    public JFreeChart createChart(int choice) {
+    		readFile r;
+    		
+    		switch (choice) {
+    	      
+    		case 1: 
+    			fileLoc = "C:\\Users\\haseebchaudhry\\git\\repository\\stockAnalysisProgram\\src\\Data\\Daily - 6 months.txt";
+    			r = new readFile(fileLoc);
+    			f = new formulas(fileLoc);
+    			r.makeArray(); 
+    			f.createValues(r.getArray());
+    			f.createDateArray(r.getArray());
+    			f.createSMA(frame);
+      				period = 130;
+      				title = "daily - 6 months"; 
+      				break;
+    		case 2: 
+    			fileLoc = "C:\\Users\\haseebchaudhry\\git\\repository\\stockAnalysisProgram\\src\\Data\\Weekly - 2 years.txt";
+    			r = new readFile(fileLoc);
+    			f = new formulas(fileLoc);
+    			r.makeArray(); 
+    			f.createValues(r.getArray());
+    			f.createDateArray(r.getArray());
+    			f.createSMA(frame);
+    	  			period = 106;
+    	  			title = "weekly - 2 years";
+    	  			break;
+    		case 3: 
+    			fileLoc = "C:\\Users\\haseebchaudhry\\git\\repository\\stockAnalysisProgram\\src\\Data\\Monthly - 5 years.txt";
+    			r = new readFile(fileLoc);
+    			f = new formulas(fileLoc);
+    			r.makeArray(); 
+    			f.createValues(r.getArray());
+    			f.createDateArray(r.getArray());
+    			f.createSMA(frame);
+      				period = f.values.length;
+      				title = "monthly - 5 years";
+      				break;
+    	}
         // create subplot 1...
         final XYDataset data1 = createDataset1(f.getDates(), f.getValues());
         final XYItemRenderer renderer1 = new StandardXYItemRenderer();
+        	renderer1.setSeriesPaint( 0 , Color.BLACK );
+	    	renderer1.setSeriesStroke( 0 , new BasicStroke( 1f ) );
         final NumberAxis rangeAxis1 = new NumberAxis("Range 1");
         final XYPlot subplot1 = new XYPlot(data1, null, rangeAxis1, renderer1);
-        subplot1.setRangeAxisLocation(AxisLocation.BOTTOM_OR_LEFT);
-        
-       
+        	subplot1.setRangeAxisLocation(AxisLocation.BOTTOM_OR_LEFT);
    
         // create subplot 2...
         final XYDataset data2 = createDataset2();
         final XYItemRenderer renderer2 = new StandardXYItemRenderer();
+        	renderer2.setSeriesPaint(0, Color.CYAN);
+        	renderer2.setSeriesPaint(1, Color.BLACK);
         final NumberAxis rangeAxis2 = new NumberAxis("Range 2");
         final XYPlot subplot2 = new XYPlot(data2, null, rangeAxis2, renderer2);
-        subplot2.setRangeAxisLocation(AxisLocation.TOP_OR_LEFT);
+        	subplot2.setRangeAxisLocation(AxisLocation.TOP_OR_LEFT);
         
         //create subplot 3...
         final XYDataset data3 = createDataset3();
         final XYItemRenderer renderer3 = new StandardXYItemRenderer();
-        final NumberAxis rangeAxis3 = new NumberAxis("Range 2");
+        	renderer3.setSeriesPaint( 0 , Color.BLACK );
+	    	renderer3.setSeriesStroke( 0 , new BasicStroke( 1f ) );
+        final NumberAxis rangeAxis3 = new NumberAxis("Range 3");
         final XYPlot subplot3 = new XYPlot(data3, null, rangeAxis3, renderer3);
+        	subplot3.setRangeAxisLocation(AxisLocation.TOP_OR_LEFT);
         
-        subplot2.setRangeAxisLocation(AxisLocation.TOP_OR_LEFT);
-        
-   
 	    //formating xAxis as months
 	    DateAxis xAxis = new DateAxis ("Date");
 	    //from start of array(2013) to end of array(2018)
 	    xAxis.setRange(f.dateArray[frame], f.dateArray[f.dateArray.length-1]);
 	    xAxis.setDateFormatOverride(new SimpleDateFormat("MMM"));
 	    xAxis.setTickMarkPosition(DateTickMarkPosition.MIDDLE);
-	    
 	    
         // parent plot...
         final CombinedDomainXYPlot plot = new CombinedDomainXYPlot(new NumberAxis("Domain"));
@@ -103,10 +132,11 @@ public class subPlot extends ApplicationFrame {
         plot.setDomainAxis(xAxis);
         plot.setOrientation(PlotOrientation.VERTICAL);
 
-        // return a new chart containing the overlaid plot...
-        return new JFreeChart("CombinedDomainXYPlot Demo",
-                              JFreeChart.DEFAULT_TITLE_FONT, plot, true);
-
+        JFreeChart chart = new JFreeChart(title,
+                JFreeChart.DEFAULT_TITLE_FONT, plot, true);
+       // ChartUtilities.applyCurrentTheme(chart);
+        
+		return chart;
     }
 
     private XYDataset createDataset1(Date d [], double x [][])  {
@@ -142,7 +172,7 @@ public class subPlot extends ApplicationFrame {
     private XYDataset createDataset2() {
 
         // create dataset 2...
-        final XYSeries series2 = new XYSeries("Series 3");
+        final XYSeries series2 = new XYSeries("MACD");
 
         series2.add(10.0, 16853.2);
         series2.add(20.0, 19642.3);
@@ -166,7 +196,7 @@ public class subPlot extends ApplicationFrame {
     private XYDataset createDataset3() {
 
         // create dataset 3...
-        final XYSeries series2 = new XYSeries("Series 3");
+        final XYSeries series2 = new XYSeries("Fast Stochastic");
 
         series2.add(10.0, 16853.2);
         series2.add(20.0, 19642.3);
@@ -187,12 +217,20 @@ public class subPlot extends ApplicationFrame {
 
   }
     public static void main(final String[] args) {
-
-        final subPlot demo = new subPlot("CombinedDomainXYPlot Demo");
-        demo.pack();
-        RefineryUtilities.centerFrameOnScreen(demo);
-        demo.setVisible(true);
-
-    }
+  	  Scanner console = new Scanner(System.in);
+	     int choice = console.nextInt();
+	     
+	  subPlot chart = new subPlot(
+      "ABX" ,
+      choice);
+	  
+	  
+	  
+	  chart.pack();
+	  RefineryUtilities.centerFrameOnScreen( chart );
+	  chart.setVisible( true );
+   
+	  console.close();
+}
 
 }
